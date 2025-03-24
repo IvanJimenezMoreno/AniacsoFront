8c31106bc785cc04fbf27af6176aece1ae6863f6
Manuel <2emmaestrejimenez@gmail.com>
2025-03-24 17:29:42 +0100

    Pagina inicial y formulario

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { RegistroComponent } from './registro/registro.component';
 
 export const routes: Routes = [
-  {path: 'inicio', component: InicioComponent}
+  {path: '', component: InicioComponent}, 
+  {path: 'iniciosesion', component: InicioSesionComponent},
+  {path: 'registro', component: RegistroComponent}
 ];
