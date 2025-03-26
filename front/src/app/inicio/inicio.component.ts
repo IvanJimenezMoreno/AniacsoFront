7520e1f798bfb8f09cd2545e74762209a23c7517
Ivan <ivanjimenez1101@gmail.com+>
2025-03-26 12:06:31 +0100

    corregido

--- front/src/app/inicio/inicio.component.ts
+++ front/src/app/inicio/inicio.component.ts
@@ -21,15 +21,16 @@ export class InicioComponent implements OnInit {
   RegistroService: any;
   mensajeError: string | null = null;
   mensajeExito: string | null = null;
-  errorValidacion: { [key: string]: string[] } | null = {};
+  passwordErrors: string[] = [];
+  
 
   constructor(private formBuilder: FormBuilder, private registroService: RegistroService) {
     this.formulario = this.formBuilder.group({
       nombre: ['', [Validators.required, Validators.maxLength(255)]],
       apellidos:['',[Validators.required,Validators.maxLength(255)]],
       email:['',[Validators.required,Validators.email]],
-      contraseña:['',[Validators.required,Validators.minLength(8)]],
-      contraseña_confirmation:['',[Validators.required,Validators.minLength(8)]],
+      password:['',[Validators.required,Validators.minLength(8)]],
+      password_confirmation:['',[Validators.required,Validators.minLength(8)]],
       apodo:['',[Validators.required,Validators.maxLength(255)]],
       frase:['',[Validators.required,Validators.maxLength(255)]]
       
@@ -51,14 +52,17 @@ export class InicioComponent implements OnInit {
       if(frase.toUpperCase() === respuesta){
         this.registroService.registro(datosSinFrase).subscribe(response => {
           console.log('Respuesta de la API:', response);
+          this.mensajeError = null;
+          this.passwordErrors = [];
+          this.mensajeExito = '¿Quién eres? ¿Cómo has conseguido dar con la clave? (Ve para arriba, ya puedes iniciar sesión)';
         }, error => {
           console.error('Error al llamar a la API:', error);
-          this.mensajeError = error.message;
-          this.errorValidacion = this.transformValidationErrors(error.error.errores || null);
+          if (error.error.errores && error.error.errores.password) {
+            this.passwordErrors = error.error.errores.password;
+          } else {
+            this.mensajeError = error.message;
+          }
         });
-        this.mensajeError = null 
-        this.errorValidacion = null
-        this.mensajeExito = '¿Quién eres? ¿Cómo has conseguido dar con la clave?'
       } else{
           this.mensajeError = "No puedes, simplemente no puedes registrate";
           this.mensajeExito = null
@@ -67,14 +71,4 @@ export class InicioComponent implements OnInit {
     }
   }
 
-  private transformValidationErrors(errors: any): any {
-    const transformedErrors: any = {};
-    for (const key in errors) {
-      if (errors.hasOwnProperty(key)) {
-        const newKey = key.replace('ñ', 'n');
-        transformedErrors[newKey] = errors[key];
-      }
-    }
-    return transformedErrors;
-  }
 }
