9a76ffc60214e799373a93d8ad08333b166103c7
Ivan <ivanjimenez1101@gmail.com+>
2025-03-27 15:48:15 +0100

    Home

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
-  {path: '', component: InicioComponent}, 
+  {path: '', component: InicioComponent},
   {path: 'iniciosesion', component: InicioSesionComponent},
-  
+  {path: 'home', component: HomeComponent},
+  // {path: '**', component:InicioComponent}
+
 ];
