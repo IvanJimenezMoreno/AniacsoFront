1a4c03f626100b675b358ae89b54108aef600e26
Manuel <2emmaestrejimenez@gmail.com>
2025-04-09 14:52:29 +0200

    creacion miembros

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 import { HomeComponent } from './home/home.component';
+import { MiembrosComponent } from './miembros/miembros.component';
 
 
 export const routes: Routes = [
   {path: '', component: InicioComponent},
   {path: 'iniciosesion', component: InicioSesionComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'miembros', component: MiembrosComponent},
   // {path: '**', component:InicioComponent}
 
 ];
---
+++ front/src/app/miembros/miembros.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-miembros',
+  imports: [CommonModule],
+  templateUrl: './miembros.component.html',
+  styleUrl: './miembros.component.css'
+})
+export class MiembrosComponent {
+
+}
